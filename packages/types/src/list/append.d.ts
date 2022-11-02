7fcb97a7fb6daee03f733ec42f9289bf1a6ac4c4
william duan <38791932+duanwilliam@users.noreply.github.com>
2022-11-01 19:21:22 -0700

    feat(types): allow list `Append` to append as optional

--- packages/types/src/list/append.d.ts
+++ packages/types/src/list/append.d.ts
@@ -1,6 +1,13 @@
+import { Bool } from "../bool/bool";
+import { If } from "../bool/if";
 import { List } from "./list";
 
 /**
  * resultant list from appending `X` to the end of list `A`
  */
-export type Append<A extends List, X> = [...A, X]
+export type Append<A extends List, X, Optional extends Bool = 0> =
+  If<
+    Optional,
+    [...A, X?],
+    [...A, X]
+  >
