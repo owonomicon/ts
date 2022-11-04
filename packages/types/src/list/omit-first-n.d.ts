4a5d467c0127e872a0a0395eba9ae6ea4340b2ab
william duan <38791932+duanwilliam@users.noreply.github.com>
2022-11-04 01:49:55 -0700

    fix(types): handle variadic tuples in `OmitFirstN`

--- packages/types/src/list/omit-first-n.d.ts
+++ packages/types/src/list/omit-first-n.d.ts
@@ -4,21 +4,23 @@ import { Length } from "./length"
 import { List } from "./list"
 
 type _OmitFirstN<T extends List, N extends number, A extends Iteration = Iterate<0>> =
-  Length<T> extends 0
-    ? T
-    : T extends [any?, ...infer V] | readonly [any?, ...infer V]
-      ? Value<A> extends N
-        ? T
-        : _OmitFirstN<V, N, Increment<A>>
-      : never
+  Value<A> extends N ? T
+  : Length<T> extends 0 ? T
+  : T extends [any, ...infer Tail] | readonly [any, ...infer Tail] ? _OmitFirstN<Tail, N, Increment<A>> // capture nonempty lists that aren't <variadic tuples with leading spread element>
+  : T extends [...any, any] | readonly [...any, any] ? T // capture variadic tuples with leading spread element
+  : T extends [any?, ...infer Tail] | readonly [any?, ...infer Tail] ? _OmitFirstN<Tail, N, Increment<A>> // capture nonempty lists that aren't <variadic tuples with leading spread element> and have optional argument. this comes after leading spread variadic tuple because this "matches" leading spread variadic tuples but converts them to `unknown[]`
+  : never
 
 /**
- * omits the first `N` elements of tuple `T`
- * @warning doesn't seem to work with variadic tuples
+ * omits the first `N` elements of list `T`.
+ * works with arbitrary lists, variadic tuples, etc.
+ * does not do anything to spread elements encountered in the front.
  */
-export type OmitFirstN<T extends List, N extends number> = _OmitFirstN<T, N>
+export type OmitFirstN<L extends List, N extends number> = _OmitFirstN<L, N>
 
 /**
- * omits the first element of tuple `T`
+ * omits the first element of list `L`.
+ * 
+ * @see {@link OmitFirstN}
  */
-export type OmitFirst<T extends List> = OmitFirstN<T, 1>
+export type OmitFirst<L extends List> = OmitFirstN<L, 1>
