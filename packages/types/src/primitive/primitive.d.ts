8bbceb935365f26d1b7667ca04b977f7b8b1321a
william duan <38791932+duanwilliam@users.noreply.github.com>
2022-11-03 23:35:24 -0700

    refactor(types): reorder types in `Primitive` union type

--- packages/types/src/primitive/primitive.d.ts
+++ packages/types/src/primitive/primitive.d.ts
@@ -3,10 +3,11 @@
  * @see https://developer.mozilla.org/en-US/docs/Glossary/Primitive
  */
 export type Primitive =
+  | symbol
   | string
   | number
+  | bigint
   | boolean
   | null
   | undefined
-  | symbol
-  | bigint
+  
