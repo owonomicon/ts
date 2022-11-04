8dfaabb83ea066549b462a95c860050c30e00887
william duan <38791932+duanwilliam@users.noreply.github.com>
2022-11-03 23:26:43 -0700

    feat(types): add `Unreachable` type

---
+++ packages/types/src/_meta/unreachable.d.ts
@@ -0,0 +1,9 @@
+import { Nominal } from "./nominal";
+
+declare const __UNREACHABLE__: unique symbol
+
+/**
+ * a type to flag a branch of a conditional type to be unreachable.
+ * mostly useful for grepping.
+ */
+export type Unreachable = Nominal<{}, typeof __UNREACHABLE__>
