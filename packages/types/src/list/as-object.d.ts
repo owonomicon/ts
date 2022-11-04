dda6090f8c57d95d4624fbe155c41ad51d228b9b
william duan <38791932+duanwilliam@users.noreply.github.com>
2022-11-04 01:59:38 -0700

    fix/refactor(types): rewrite list `AsObject`
    - fix a lot of bugs with list `AsObject`
        - make it behave properly with stuff like variadic tuples

--- packages/types/src/list/as-object.d.ts
+++ packages/types/src/list/as-object.d.ts
@@ -1,16 +1,43 @@
 import { If } from "../bool/if";
-import { IsTuple } from "./is-tuple";
+import { Increment, Iterate, Iteration, Value } from "../_meta/iterate";
+import { Resolve, ShallowResolve } from "../_meta/resolve";
+import { Unreachable } from "../_meta/unreachable";
+import { ElementOf } from "./element-of";
+import { IsFiniteList } from "./is-finite-list";
 import { List } from "./list";
+import { OmitFirstN } from "./omit-first-n";
+
+/**
+ * get the "length" of the resultant named object from a list
+ */
+type _AsObject<A extends List, Named, N extends Iteration = Iterate<0>> =
+  Value<Increment<N>> extends keyof Named ? _AsObject<A, Named, Increment<N>>
+  : `${Value<Increment<N>>}` extends keyof Named ? _AsObject<A, Named, Increment<N>>
+  : Value<Increment<N>>
 
 /**
  * coerces an array type into an object.
  * this can be useful for then e.g. detecting optional arguments in the parameters of a function
+ * 
+ * @example
+ * type e0 = AsObject<[]> // {}
+ * type e1 = AsObject<[string, number, object]> // { 0: string, 1: number, 2: object }
+ * 
+ * type e2 = AsObject<[string, number, object, ...boolean[]]> // { [x: number]: boolean, 0: string, 1: number, 2: object }
+ * type e2a = e2[0] // string
+ * type e2b = e2[3] // boolean
+ * 
+ * type e3 = AsObject<[string, number, object, ...boolean[], null]> // { [x: number]: boolean | null, 0: string, 1: number, 2: object } // all types following the first spread of a variadic tuple are combined
+ * 
+ * type e4 = AsObject<[string, number, object?, ...boolean[]] // { [x: number]: boolean, 0: string, 1: number, 2?: object | undefined } // optionality is preserved (and TypeScript converts `T?` to `(T | undefined)?`)
  */
-export type AsObject<A extends List> = 
-  If<
-    IsTuple<A>,  
-    Omit<A, keyof any[]>,
-    Pick<A, number>
+export type AsObject<A extends List> =
+  ShallowResolve<
+    If<
+      IsFiniteList<A>,  
+      Omit<A, keyof any[]>,
+      Omit<A, keyof any[]> extends infer X
+        ? { [x: number]: ElementOf<OmitFirstN<A, _AsObject<A, X>>> } & X
+        : Unreachable
+    >  
   >
-
-type o = AsObject<[string, number, object?, ...null[]]> // TODO: capture spread arguments. maybe some way to detect largest index without spread, and construct a [x in number]: x extends (anything < first spread index) ? A[x] : [the spread]
