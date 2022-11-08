9f30d64511cb4676ec458d8cb15ab5fcf294c702
william duan <38791932+duanwilliam@users.noreply.github.com>
2022-11-08 11:18:32 -0800

    docs(types): add examples to `IsUniqueSymbol` type

--- packages/types/src/any/is-unique-symbol.d.ts
+++ packages/types/src/any/is-unique-symbol.d.ts
@@ -6,6 +6,13 @@ import { Extends } from "../_meta/extends";
 /**
  * whether `T` is an instance of a unique symbol.
  * to reinforce the uniqueness of a unique symbol, also asserts that `T` is in fact strictly a unique symbol and is not a union type
+ * 
+ * @example
+ * declare const a: unique symbol
+ * declare const b: unique symbol
+ * type e0 = IsUniqueSymbol<typeof a>             // 1
+ * type e1 = IsUniqueSymbol<symbol>               // 0
+ * type e2 = IsUniqueSymbol<typeof a | typeof b>  // 1
  */
 export type IsUniqueSymbol<T> =
   And<
