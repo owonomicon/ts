4366b6ebf4ebf0617d29d3c937d2fa13e0bafe2d
william duan <38791932+duanwilliam@users.noreply.github.com>
2022-11-08 11:24:59 -0800

    fix(types): properly handle `never` in `Extends` type

--- packages/types/src/_meta/extends.d.ts
+++ packages/types/src/_meta/extends.d.ts
@@ -1,21 +1,21 @@
 import { IsNever } from "../any/is-never";
-import { And } from "../bool/and";
-import { Not } from "../bool/not";
+import { If } from "../bool/if";
 
 /**
  * whether `A` extends `B`.
- * 
+ * if `A` or `B` are never, resolves to `0`.
  * 
  * @example
- * type e0 = Exceeds<0, 1> // 0
+ * type e0 = Extends<0, 1> // 0
  * 
  * // nothing can be `never`. use `IsNever` to check if a type is `never`
- * type n0 = Exceeds<never, never> // 0
- * type n1 = Exceeds<string, never> // 0
- * type n2 = Extends<never, string> // 0
+ * type e1 = Extends<never, never> // 0
+ * type e2 = Extends<string, never> // 0
+ * type e3 = Extends<never, string> // 0
  */
 export type Extends<A, B> =
-  And<
-    Not<IsNever<A>>,
+  If<
+    IsNever<A>,
+    0,
     A extends B ? 1 : 0
   >
