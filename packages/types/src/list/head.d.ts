0a4bd983ccb6fc93c131c177c0fee1d56da87f0e
william duan <38791932+duanwilliam@users.noreply.github.com>
2022-11-13 00:17:36 -0800

    feat(types): add option to list `Head` type to ignore optionality

--- packages/types/src/list/head.d.ts
+++ packages/types/src/list/head.d.ts
@@ -1,11 +1,17 @@
+import { Bool } from "../bool/bool";
+import { Required } from "../object/required";
 import { Length } from "./length";
 import { List } from "./list";
+
+type Options = { makeRequired: Bool }
 
 /**
  * extracts the head of list `T`.
  * if `T` is empty, resolves to `never`
  */
-export type Head<T extends List> =
+export type Head<T extends List, O extends Options = { makeRequired: 0 }> =
   Length<T> extends 0
     ? never
-    : T[0]
+    : O['makeRequired'] extends 0
+      ? T[0]
+      : Required<T>[0]
