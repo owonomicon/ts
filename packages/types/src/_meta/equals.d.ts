03d63807f88b2d9b43b52eb596b41f9cc8dfad8a
william duan <38791932+duanwilliam@users.noreply.github.com>
2022-11-26 01:09:14 -0800

    fix(types)!: fix variadic tuple edge case with `Equals`
    `Equals` failed to detect that `string[]` and `[...string[], string]` are different types.
    this commit adds a mutual assignability check (which properly distinguishes the aforementioned two types) in `Equals`.
    THIS MAY RESULT IN A REGRESSION since the mutual assignability check may have some false negatives. but I don't know of any of those yet.
    
    also, make `Equals` resolve intersections by default, with an option to not resolve them, and add documentation accordingly.
    - as current intersection resolver cannot resolve functions, there are still certain known edge cases with intersections that do not get properly handled (specifically, function intersections)

--- packages/types/src/_meta/equals.d.ts
+++ packages/types/src/_meta/equals.d.ts
@@ -1,11 +1,79 @@
+import { MutuallyAssignable } from "./mutually-assignable"
+import { ShallowResolve } from "./resolve"
+
+type Options<RI extends Boolean = Boolean> = {
+  resolve_intersections: RI
+}
+
+/**
+ * checks whether types `A` and `B` are identical.
+ * 
+ * does not resolve intersection types (e.g. `{ a: 0 } & { b: 1 }` will be considered not equal to `{ a: 0, b: 1 }`).
+ * 
+ * adapted from MattMcCutchen's answer in {@link https://github.com/Microsoft/TypeScript/issues/27024#issuecomment-421529650}.
+ * 
+ * this is modified to try to address a niche edge case (possibly didn't even exist at the time of the answer?) with variadic tuples:
+ *  with the original code, where `[...string[], string] =/= string[]` (desired) but `string[] == [...string[], string]` (not desired; and suddenly order matters?).
+ * general shortcomings with alternative (i.e. different from MattMcCutchen's) `Equals` implementations regard false positives (as far as i'm aware),
+ *  not any false negatives. `MutuallyAssignable` is one such alternative implementation, that simply checks if both types can be assigned to each other.
+ * this properly handles the aforementioned variadic tuple case, and since it doesn't have any known false negatives (afaik),
+ *  it is thus used as the check for this variadic tuple case.
+ */
+type _Equals<A, B> =
+  (<T>() => T extends A ? 1 : 0) extends (<T>() => T extends B ? 1 : 0)
+    // instead of just returning "true" here, also do mutual assignability check to validate variadic tuple
+    ? MutuallyAssignable<A, B>
+    : 0
+
 /**
  * whether types `A` and `B` are identical.
- * note that this does not work on intersection types, although this can be circumvented by calling `Properties` on each
- * @author Matt McCutchen
+ * 
+ * by default will try to resolve intersection types (e.g. convert `{ a: 0 } & { b: 1 }` into `{ a: 0, b: 1 }`).
+ * this is usually what you want. an option is provided to not do so as well.
+ * 
+ * adapted from @author Matt McCutchen
  * @see https://github.com/Microsoft/TypeScript/issues/27024#issuecomment-421529650
+ * 
+ * @warning known issues:
+ * - certain intersections don't get properly resolved, even when `resolve_intersections` is `true` (e.g. intersection of functions; see e14). as a result there may be false negatives with regards to function intersections.
+ * 
+ * @example
+ * // many of these examples taken from the thread https://github.com/Microsoft/TypeScript/issues/27024
+ * 
+ * type e0   = Equals<number, string>                                                           // 0
+ * type e1   = Equals<1, 1>                                                                     // 1
+ * type e2   = Equals<any, 1>                                                                   // 0
+ * type e3   = Equals<1 | 2, 1>                                                                 // 0
+ * type e5   = Equals<any, never>                                                               / 0
+ * type e6   = Equals<[any], [number]>                                                          // 0
+ * 
+ * // object intersection
+ * type e7   = Equals<{ x: 1 } & { y: 2 }, { x: 1, y: 2 }>                                      // 1
+ * type e8   = Equals<{ x: 1 } & { y: 2 }, { x: 1, y: 2}, { resolve_intersections: false }>     // 0
+ * 
+ * // primitive intersection
+ * type e9   = Equals<number & {}, number>                                                      // 1
+ * type e10  = Equals<number & {}, number, { resolve_intersections: false }>                    // 0
+ * 
+ * // variadic tuple
+ * type e11  = Equals<[...string[], string], string[]>                                          // 0
+ * type e11b = Equals<string[], [...string[], string]>                                          // 0
+ * type e12 = Equals<[string, ...string[]], string[]>
+ * type e12b = Equals<string[], [string, ...string[]]>
+ * 
+ * // function intersection
+ * type t13a = (x: 0, y: null) => void
+ * type t13b = (x: number, y: string) => void
+ * type t13c = t13a & t13b
+ * type t13d = t13b & t13a
+ * type e13 = Equals<t13c, t13d>                                                                // 1
+ * type e14 = Equals<t13c, t13c | t13d>                                                         // 0 // BUG: should produce 1
+ * type e15 = Equals<                                                                           // 0
+ *   { (x: 0, y: null): void; (x: number, y: null): void },
+ *   { (x: number, y: null): void; (x: 0, y: null): void }
+ * >
  */
-export type Equals<A, B> =
-  (<T>() => T extends A ? 1 : 0) extends
-    (<T>() => T extends B ? 1 : 0)
-      ? 1
-      : 0
+export type Equals<A, B, O extends Options = Options<true>> =
+  O extends Options<true>
+    ? _Equals<ShallowResolve<A>, ShallowResolve<B>>
+    : _Equals<A, B>
