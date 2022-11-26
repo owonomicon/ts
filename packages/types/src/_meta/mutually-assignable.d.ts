583209255cf164a6a5d88885118329e60bb69b8f
william duan <38791932+duanwilliam@users.noreply.github.com>
2022-11-26 01:08:42 -0800

    feat(types): add `MutuallyAssignable` type

---
+++ packages/types/src/_meta/mutually-assignable.d.ts
@@ -0,0 +1,11 @@
+/**
+ * whether types `A` and `B` are mutually assignable (i.e. `A` can be assigned to `B` and vice versa).
+ * 
+ * this is subtly different from types being "equal"/"identical".
+ * 
+ * for example, `[string, any]` is not `[string, any]` but both _are_ assignable to each other
+ */
+export type MutuallyAssignable<A, B> =
+  [A, B] extends [B, A]
+    ? 1
+    : 0
