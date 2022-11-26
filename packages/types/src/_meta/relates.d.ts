c81afd65478bfe70a628708c88d83d608c34f244
william duan <38791932+duanwilliam@users.noreply.github.com>
2022-11-26 12:03:13 -0800

    feat(types): add `mutually_assignable` option to `Relates` type
    - also, remove underscore prefix to symmetric/asymmetric relation types (they're not exported anyway so this shouldnt be a big issue)
    - rename "asymmetric" relation type to "nonsymmetric" to better reflect mathematical jargon
        - an asymmetric relation is actually a thing (`aRb -> not bRa`), whereas in `Relates` it just means `aRb` doesn't necessarily mean `bRa`

--- packages/types/src/_meta/relates.d.ts
+++ packages/types/src/_meta/relates.d.ts
@@ -1,21 +1,24 @@
 import { Equals } from "./equals"
 import { Extends } from "./extends"
+import { MutuallyAssignable } from "./mutually-assignable"
 
-type _SymmetricRelation =
+type SymmetricRelation =
   | 'equals'
+  | 'mutually_assignable'
 
-type _AsymmetricRelation =
+type NonsymmetricRelation =
   | 'extends'
 
 type Relation =
-  | _SymmetricRelation
-  | _AsymmetricRelation
-  | `${_AsymmetricRelation}->`
-  | `<-${_AsymmetricRelation}`
+  | SymmetricRelation
+  | NonsymmetricRelation
+  | `${NonsymmetricRelation}->`
+  | `<-${NonsymmetricRelation}`
   
 export type Relates<A, B, R extends Relation = 'extends'> =
   {
     'equals': Equals<A, B>
+    'mutually_assignable': MutuallyAssignable<A, B>
     'extends->': Extends<A, B>
     '<-extends': Extends<B, A>
     'extends': Relates<A, B, 'extends->'>
