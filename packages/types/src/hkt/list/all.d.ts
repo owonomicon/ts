ba2b415a80fcd163c260f69b22bda6299bb44aae
william duan <38791932+duanwilliam@users.noreply.github.com>
2022-12-04 17:53:02 -0800

    refactor(types): early return in hkt list `All`

--- packages/types/src/hkt/list/all.d.ts
+++ packages/types/src/hkt/list/all.d.ts
@@ -1,12 +1,7 @@
-import { And } from "../../bool/and";
 import { List } from "../../list/list";
 import { HKT } from "../hkt";
 import { $ } from "../$";
 import { Satisfies } from "../../_meta/satisfies";
-
-type _$All<Kind extends HKT<any, boolean>, L extends List, Acc extends boolean = true> =
-  L extends [infer H, ...infer T] ? _$All<Kind, T, And<Acc, $<Kind, H>>>
-  : Acc
 
 /**
  * checks whether all elements of list `L` satisfy predicate `F`.
@@ -14,8 +9,11 @@ type _$All<Kind extends HKT<any, boolean>, L extends List, Acc extends boolean =
  * an empty list is vacuously satisfied.
  */
 export type $All<Kind extends HKT<any, boolean>, L extends List> =
-  _$All<Kind, L>
-
+  L extends [infer H, ...infer T]
+    ? $<Kind, H> extends true
+      ? $All<Kind, T>
+      : false
+    : true
 
 export interface All<Kind extends HKT<any, boolean>> extends HKT {
   [HKT.i]: Satisfies<this[HKT._], HKT.I<Kind>[]>
