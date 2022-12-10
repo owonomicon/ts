b37e8de1c92a738b9928f92bf5c1d453a9ed504e
william duan <38791932+duanwilliam@users.noreply.github.com>
2022-12-09 22:43:55 -0800

    refactor(types): improve `FailedPredicate` type
    `FailedPredicate` wasn't really used since it didn't exactly provide much more information.
    now, can provide a message providing context about the failed predicate to the type.
    additionally, changed name of type to clearly indicate that end users should not be using `FailedPredicate` in their own code (well, unless they are making their own type with a predicate).

--- packages/types/src/_meta/failed-predicate.d.ts
+++ packages/types/src/_meta/failed-predicate.d.ts
@@ -1,9 +1,10 @@
-import { Nominal } from "./nominal";
-
 declare const __FAILED__PREDICATE__: unique symbol
 
 /**
  * a type to indicate that a condition has not been met.
  * useful for grepping, or discriminating between `never` as a possible valid type versus some condition not being fulfilled.
+ * @internal
+ * @private
  */
-export type FailedPredicate = Nominal<{}, typeof __FAILED__PREDICATE__>
+export type __nomicon_internal_type_do_not_use__FailedPredicate<Message extends string> =
+  { [__FAILED__PREDICATE__]: Message }
