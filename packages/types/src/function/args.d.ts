2f94250f1e44db10c4aa95af52449580d3da5314
william duan <38791932+duanwilliam@users.noreply.github.com>
2022-12-09 21:40:44 -0800

    fix(types): ensure parameter passed to function `Args` and `Return` types is a function

--- packages/types/src/function/args.d.ts
+++ packages/types/src/function/args.d.ts
@@ -1,10 +1,11 @@
+import { Function } from "./function";
 import { List } from "../list/list";
 import { Satisfies } from "../_meta/satisfies";
 
 /**
  * extracts the arguments of function `F`
  */
-export type Args<F> =
+export type Args<F extends Function> =
   F extends (...args: infer A) => any  // use A vs `List<A>` or `A[]` to successfully match against variadic tuple parameters
     ? Satisfies<A, List>
     : never
--- packages/types/src/function/return.d.ts
+++ packages/types/src/function/return.d.ts
@@ -1,7 +1,9 @@
+import { Function } from "./function";
+
 /**
  * gets the return type of function `F`
  */
-export type Return<F> =
+export type Return<F extends Function> =
   F extends (...args: any) => infer R // use any vs `List` or `any[]` to successfully match against variadic tuple parameters
     ? R
     : never
