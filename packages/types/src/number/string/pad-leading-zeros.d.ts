b8da1c6ecb8a3a7da1f5afb4eca21a50818f3685
william duan <38791932+duanwilliam@users.noreply.github.com>
2022-12-15 19:12:18 -0800

    feat(types): add number string `PadLeadingZeros` and `StripLeadingZeros` types

---
+++ packages/types/src/number/string/pad-leading-zeros.d.ts
@@ -0,0 +1,6 @@
+import { Length } from "../../string/length";
+
+export type PadLeadingZeros<S extends string, N extends number> =
+  Length<S> extends N
+    ? S
+    : PadLeadingZeros<`0${S}`, N>
---
+++ packages/types/src/number/string/strip-leading-zeros.d.ts
@@ -0,0 +1,11 @@
+type _StripLeadingZeros<S extends string> =
+  S extends '0' ? S // don't convert zero itself into the empty string -- we only want to strip _leading_ zeros
+  : S extends `0${infer T}` ? _StripLeadingZeros<T>
+  : S
+
+/**
+ * strips leading zeros from number string `S`
+ */
+export type StripLeadingZeros<S extends string> =
+  S extends `-${infer T}` ? `-${_StripLeadingZeros<T>}`
+  : _StripLeadingZeros<S>
