7e93537d1a640e24f7ca2696b3ecac95f3bdd5d1
william duan <38791932+duanwilliam@users.noreply.github.com>
2022-12-17 13:26:28 -0800

    chore(types): fix issue with doc comments in `MutuallyAssignable`

--- packages/types/src/_meta/mutually-assignable.d.ts
+++ packages/types/src/_meta/mutually-assignable.d.ts
@@ -3,7 +3,7 @@
  * 
  * this is subtly different from types being "equal"/"identical".
  * 
- * for example, `[string, any]` is not `[string, any]` but both _are_ assignable to each other
+ * for example, `[string, any]` is not `[any, string]` but both _are_ assignable to each other
  */
 export type MutuallyAssignable<A, B> =
   [A, B] extends [B, A]
