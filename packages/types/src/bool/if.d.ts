4539328142a3cd722d21c2d77b798c4e19b324c5
william duan <38791932+duanwilliam@users.noreply.github.com>
2022-12-30 11:12:46 -0800

    refactor(types)!: remove type parameter in `If`
    `If` formerly had an optional fourth type parameter to determine what constituted as "true" in the `extends` condition.
    this was in practice never used anywhere.
    furthermore, since many other parts of the library (e.g. `IsWhatever`, `And`, `Or`, etc) operate on booleans,
    it's practical to have `If` be the same.
    
    BREAKING CHANGE: `If` now always tests the condition against `true`
    To migrate:
    In the unlikely event the fourth type parameter `True` was used anywhere,
    remove it and replace the condition with `Extends<Condition, True>`

--- packages/types/src/bool/if.d.ts
+++ packages/types/src/bool/if.d.ts
@@ -3,14 +3,12 @@
  * @template Condition the type to evaluate
  * @template Then resultant type if `Condition` evaluates to `True`
  * @template Else resultant type if `Condition<A, B>` does not evaluate to `True`
- * @template True: type representing "true" for `Condition` (i.e. `Condition` extends `True`)
  */ 
 export type If<
   Condition,
   Then = Condition,
   Else = never,
-  True = true
 > =
-  Condition extends True
+  Condition extends true
     ? Then
     : Else
