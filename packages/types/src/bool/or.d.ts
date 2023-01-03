0cd75d49e4286a50303fc34673f8524c7b478526
william duan <38791932+duanwilliam@users.noreply.github.com>
2023-01-02 18:18:23 -0800

    fix(types): use nonbroken implementation of `Or`
    the old implementation of `Or` appears to have a strange bug relating to type evaluation
        when certain generic types are passed in.
    a github issue has been filed regarding this: https://github.com/microsoft/TypeScript/issues/52068
    in the meantime, a separate implementation that doesnt see this bug has replaced the original.
    this separate implementation, like the original, does not distribute the types

--- packages/types/src/bool/or.d.ts
+++ packages/types/src/bool/or.d.ts
@@ -2,6 +2,13 @@
  * checks if either `A` or `B` do not extend `false`
  */
 export type Or<A extends boolean, B extends boolean> =
-  [A, B] extends [false, false]
-    ? false
-    : true
+  /* there appears to be some strange behavior where this construct returns the wrong value
+   *  when used in a generic type, and either `A` or `B` use that generic type's type parameter in turn.
+   * @see https://github.com/microsoft/TypeScript/issues/52068 
+   */
+  // [A, B] extends [false, false]
+  //   ? false
+  //   : true
+  [A] extends [true] ? true
+  : [B] extends [true] ? true
+  : false
