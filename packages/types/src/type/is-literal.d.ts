d62e82df5b5d960017ea6efb684c82a9d46e22da
william duan <38791932+duanwilliam@users.noreply.github.com>
2023-01-02 18:13:06 -0800

    refactor(types): defer type evaluation in `IsLiteral` type
    `IsLiteral` used to eagerly evaluate `IsNumberLiteral` and `IsStringLiteral` and logical OR them.
    to slightly improve performance, it now first checks if `T` is a number literal,
    then only checks if `T` is a string literal if it is not a number literal

--- packages/types/src/type/is-literal.d.ts
+++ packages/types/src/type/is-literal.d.ts
@@ -1,4 +1,3 @@
-import { Or } from "../bool/or"
 import { IsNumberLiteral } from "../number/is-number-literal"
 import { IsLiteral as IsStringLiteral } from "../string/is-literal"
 
@@ -6,9 +5,6 @@ import { IsLiteral as IsStringLiteral } from "../string/is-literal"
  * whether `T` is a literal type (string or number literal)
  */
 export type IsLiteral<T> =
-  Or<
-    T extends string
-      ? IsStringLiteral<T>
-      : false,
-    IsNumberLiteral<T>
-  >
+  IsNumberLiteral<T> extends true ? true
+  : IsStringLiteral<T> extends true ? true
+  : false
