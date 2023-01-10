557de0449ee20deebc4bf8edc3852a1a9df61388
william duan <38791932+duanwilliam@users.noreply.github.com>
2023-01-09 20:00:13 -0800

    refactor(types)!: make `TS_TYPE_ERROR` private, add comments regarding it
    `TS_TYPE_ERROR` had a very long prefix to try to warn users to not use it.
    this unfortunately made it unwieldy to use, and also made error messages using it more cryptic.
    as such, it has been made unexported, its prefix removed, and comments have been added
    explaining why it's unexported and elaborating its purpose as a reference implementation
    and its uses.
    
    BREAKING CHANGE:
    the ts type error type has been unexported.
    to migrate:
    you shouldn't have ever used it anyway.
    but if in the offchance you did just create your own TS_TYPE_ERROR type
    based on its implementation wherever it was used.

--- packages/types/src/type/ts-type-error.d.ts
+++ packages/types/src/type/ts-type-error.d.ts
@@ -1,10 +1,18 @@
 declare const TS_TYPE_ERROR: unique symbol
 
 /**
- * a type to indicate that a condition has not been met.
- * useful for grepping, or discriminating between `never` as a possible valid type versus some condition not being fulfilled.
+ * an example of creating an error type that cannot be accidentally assigned to.
+ * 
+ * essentially, by creating an unexported unique symbol key, it's impossible to create the type on the user end.
+ * 
+ * @remark
+ * it's also for this reason that this type isn't exported - otherwise, users would be able to construct this type.
+ * as such, this serves simply as a reference implementation for creating type error types within a file
+ * 
+ * @remark
+ * the name of the type DOES matter, as it's what appears for custom type parameter constraints.
+ * 
  * @internal
- * @private
  */
-export type __nomicon_internal_type_do_not_use__TS_TYPE_ERROR<Message extends string> =
+type TS_TYPE_ERROR<Message extends string> =
   { [TS_TYPE_ERROR]: Message }
