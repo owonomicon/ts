e216965c240247b7d12bc4abc023bd17c54cab0e
william duan <38791932+duanwilliam@users.noreply.github.com>
2023-01-14 11:21:53 -0800

    refactor/comment(types): change `OptionalKeys` implementation and add comments
    `OptionalKeys` didn't behave properly with index signatures, and was coupled to `RequiredKeys`.
    it now no longer depends on `RequiredKeys`.
    it still has issues when an optional key is assignable to an index signature.
    this issue has been documented in the comments.

--- packages/types/src/object/optional-keys.ts
+++ packages/types/src/object/optional-keys.ts
@@ -1,14 +1,24 @@
-import { Exclude } from "../set-theory/exclude"
-import { RequiredKeys } from "./required-keys"
+import { Unreachable } from "../type/unreachable"
+import { Required } from "./required"
 
 /**
  * gets the optional keys of `T`
+ * 
+ * @remarks
  * does not capture index signatures.
  *
+ * @remarks
+ * may fail to capture keys that are also assignable to an index signature
+ * 
  * @example
  * type e0 = OptionalKeys<{}> // never
  * type e1 = OptionalKeys<{ a?: 0 }> // "a"
  * type e2 = OptionalKeys<{ a?: 0, b: 1 | undefined, c?: 2 }> // "a" | "c"
+ * 
+ * type w0 = OptionalKeys<{ [x: number]: string, 0?: '' }>  // never // should've captured 0!!
  */
 
-export type OptionalKeys<T> = Exclude<keyof T, RequiredKeys<T>>
+export type OptionalKeys<T> =
+  Required<T> extends (infer U extends Record<keyof T, any>)
+    ? { [K in keyof T]-?: T[K] extends U[K] ? never : K }[keyof T]
+    : Unreachable
