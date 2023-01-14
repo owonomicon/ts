fcad7508f77987f55b1c369c5da0a0f1aa67f019
william duan <38791932+duanwilliam@users.noreply.github.com>
2023-01-14 11:44:06 -0800

    fix/comment(types): handle variadic tuples with leading optional in `IsTuple`
    `IsTuple` erroneously marked variadic tuples with leading optionals as not tuples.
    this fixes that at the expense of just a single additional conditional check.
    
    additionally, add details and examples to doc comments for `IsTuple`

--- packages/types/src/list/is-tuple.ts
+++ packages/types/src/list/is-tuple.ts
@@ -2,10 +2,29 @@ import { List } from "./list"
 
 /**
  * whether `L` is a tuple type (including readonly)
+ * 
+ * @undefined_behavior `L` is never
+ * 
+ * @time_complexity O(1)
+ * 
+ * @since 0.1.0
+ * 
+ * @example
+ * ```ts
+ * type e0 = IsTuple<any[]>                 // false
+ * type e1 = IsTuple<[any?, ...any[]]>      // true
+ * type e2 = IsTuple<[any, any?, ...any[]]> // true
+ * type e3 = IsTuple<[...any[], any]>       // true
+ * type e4 = IsTuple<[any, ...any[], any]>  // true
+ * ```
  */
 export type IsTuple<L extends List> =
   any[] extends L
-    ? false
+    // variadic tuple with leading optional still passes the earlier conditional,
+    // so we need to handle that case here.
+    ? [L] extends [{ 0?: any }]
+      ? true
+      : false
     : true
 
 export type IsTupleList<T> =
