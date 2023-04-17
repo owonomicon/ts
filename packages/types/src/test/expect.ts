27170b46673c4076090a803670aff684cffad825
william duan <38791932+duanwilliam@users.noreply.github.com>
2023-04-17 01:01:40 -0700

    refactor(types): remove dependencies for `Expect`
    substitute the `IsNever` dependency with its implementation in `Expect`.
    should also make it marginally faster.

--- packages/types/src/test/expect.ts
+++ packages/types/src/test/expect.ts
@@ -1,5 +1,3 @@
-import { IsNever } from "../type"
-
 /**
  * type that errors if `T` is not `true`. the return type is irrelevant and should not be used.
  * 
@@ -16,14 +14,14 @@ import { IsNever } from "../type"
  *  Expect<true>,
  * 
  *  // should error on false
- *  // \@ts-expect-error // vscode intellisense doesn't seem to handle `// @ts-expect-error` in the doc comments very well...
+ *  // @ts-expect-error // vscode intellisense doesn't seem to handle `// @ts-expect-error` in the doc comments very well...
  *  Expect<false>,
  * ]
  * ```
  */
 export type Expect<T extends true> =
   // `never` always passes the type parameter constraint, so force a ts2589 error here by inducing infinite recursion 
-  IsNever<T> extends true ? Expect<T>
+  [T] extends [never] ? Expect<T>
   // same with `any` and `unknown`
   : unknown extends T ? Expect<T>
   : never
@@ -32,50 +30,38 @@ export type Expect<T extends true> =
 module Tests {
   type MutuallyAssignable<A, B> = import('../type/mutually-assignable').MutuallyAssignable<A, B>
 
-  type Tests = [
-    /**
-     * should pass on true
-     */
+  type Test = [
+    /* should pass on `true` */
     Expect<true>,
 
-    /**
-     * should error on false
-     */
+    /* should error on `false` */
     // @ts-expect-error
     Expect<false>,
-    
-    /**
-     * should error on non-booleans
-     */
+
+    /* should error on `boolean` */
+    // @ts-expect-error
+    Expect<boolean>,
+
+    // should error on non-boolean number type */
     // @ts-expect-error
     Expect<1>,
-    
-    /**
-     * should pass on generic evaluating to true
-     */
+
+    /* should pass on generic evaluating to `true` */
     Expect<MutuallyAssignable<1, 1>>,
-    
-    /**
-     * should error on generic evaluating to false
-     */
+
+    /* should error on generic evaluating to `false` */
     // @ts-expect-error
     Expect<MutuallyAssignable<1, 0>>,
-    
-    /**
-     * should error on never
-     */
+
+    /* should error on `never` */
     // @ts-expect-error
     Expect<never>,
-
-    /**
-     * should error on any
-     */
+  
+    /* should error on `any` */
     // @ts-expect-error
     Expect<any>,
-
-    /**
-     * should error on unknown
-     */
+  
+    /* should error on `unknown` */
     // @ts-expect-error
     Expect<unknown>,
   ]
