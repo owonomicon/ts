c6a9552ccb3b2a1e9772de6472fe7ecfbb2b5dd0
william duan <38791932+duanwilliam@users.noreply.github.com>
2023-08-01 21:51:41 -0700

    fix(types): properly handle trailing spreads for list `Last`

--- packages/types/src/list/last.ts
+++ packages/types/src/list/last.ts
@@ -13,16 +13,15 @@ import { Length, List } from "."
  * type e4 = Last<[...string[], number]>          // number
  * type e5 = Last<[string, ...number[]]>          // number
  * type e6 = Last<[string, ...number[], boolean]> // boolean
- * type e7 = Last<[string?]>                      // string
- * type e8 = Last<[string?, ...number[]]>         // number
+ * type e7 = Last<[string?]>                      // string | undefined
+ * type e8 = Last<[string?, ...number[]]>         // number | string | undefined
  */
 export type Last<L extends List> =
+  _Last<L>
+
+type _Last<L extends List, Previous = never> = 
   Length<L> extends 0 ? never
-  : L extends readonly [_?: infer X] ? X
-  : L extends readonly [any, ...infer T] ? Last<T>
+  : L extends readonly [_: infer T] ? T
+  : L extends readonly [infer H, ...infer T] ? _Last<T, H>
   : L extends readonly [...any, infer X] ? X
-  : L extends { 0?: any} ?
-    L extends readonly [any?, ...infer T]
-      ? Last<T>
-      : L[number]
-  : L[number]
+  : L[number] | Previous
