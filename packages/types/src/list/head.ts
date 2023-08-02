5fe4ebf074a165144e46b9ab55a4f66fe640d563
william duan <38791932+duanwilliam@users.noreply.github.com>
2023-08-01 21:52:44 -0700

    refactor!(types): remove optional option for list `Head`

--- packages/types/src/list/head.ts
+++ packages/types/src/list/head.ts
@@ -1,15 +1,10 @@
-import { Required } from "../object"
 import { Length, List } from "."
-
-type Options<MR extends boolean = false> = { make_required: MR }
 
 /**
  * extracts the head of list `L`.
  * if `L` is empty, resolves to `never`
  */
-export type Head<L extends List, Opts extends Options = Options<false>> =
+export type Head<L extends List> =
   Length<L> extends 0
     ? never
-    : Opts extends Options<false>
-      ? L[0]
-      : Required<L>[0]
+    : L[0]
