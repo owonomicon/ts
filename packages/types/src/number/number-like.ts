97045afd77ed05d9b785f2f2c6d5b5b331ed7f2d
william duan <38791932+duanwilliam@users.noreply.github.com>
2023-10-01 16:23:35 -0700

    refactor(types)!: do not include bigints in `NumberLikeSerializable` by default

--- packages/types/src/number/number-like.ts
+++ packages/types/src/number/number-like.ts
@@ -1,10 +1,16 @@
 /**
  * types that can be represented as a number string
  * 
+ * @remarks
+ * 
+ * many existing apis may only expect `string` or `number` types.
+ * as such, by default, `NumberLikeSerializable` does _not_ include `bigint`s.
+ * to include `bigint`, pass it in as a generic, i.e. `NumberLikeSerializable<bigint>` 
+ * 
  * @since 0.0.6
  */
-export type NumberLikeSerializable =
+export type NumberLikeSerializable<Bigint extends bigint = never> =
   | number
-  | bigint
   | `${number}`
-  | `${bigint}`
+  | Bigint
+  | `${Bigint}` // `\`${never}\`` resolves to `never`
