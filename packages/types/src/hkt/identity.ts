dfd29363e356c7758e2f4caf01e1ac5f4bcf3089
william duan <38791932+duanwilliam@users.noreply.github.com>
2023-12-22 19:47:56 -0800

    feat(package:types): add hkt `Identity.Satisfies` hkt type

--- packages/types/src/hkt/identity.ts
+++ packages/types/src/hkt/identity.ts
@@ -1,11 +1,24 @@
 import { HKT, _, I } from "."
+import { Satisfies as _Satisfies } from "../type"
 
-/**
- * @since 0.0.2
- */
 export declare namespace Identity {
+  /**
+   * identity of type `T`
+   * 
+   * @since 0.0.2
+   */
   interface Of<T> extends HKT {
     [HKT.i]: T
+    [HKT.o]: I<this>
+  }
+
+  /**
+   * identity of type that satisfies `T`
+   * 
+   * @since 0.0.9
+   */
+  interface Satisfies<T> extends HKT {
+    [HKT.i]: _Satisfies<_<this>, T>
     [HKT.o]: I<this>
   }
 }
